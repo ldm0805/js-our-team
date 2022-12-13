a60349d5407e976278110985976c3873f293fbb7
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2022-12-13 14:16:38 +0100

    Primo push, array creato

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let member = [
+    {
+    name: "Wayne Barnett",
+    role: "Founder & Ceo",
+    image: "wayne-barnett-founder-ceo.jpg",
+    },
+    {
+    name: "Angela Caroll",
+    role: "Chief Editor",
+    image: "angella-caroll-chief-editor.jpg",
+    },
+    {
+    name: "Walter Gordon",
+    role: "Office Menager",
+    image: "walter-gordon-office-menager.jpg",
+    },
+    {
+    name: "Angela Lopez",
+    role: "Social Media Menager",
+    image: "angela-lopez-social-media-menager",
+    },
+    {
+    name: "Scott Estrada",
+    role: "Developer",
+    image: "scott-estrada-developer.jpg",
+    },
+    {
+    name: "Barbara Ramos",
+    role: "Graphic Designer",
+    image: "barbara-ramos-graphic-designer",
+    },
+]
+
