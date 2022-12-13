21d180ccb93f5457192641506b0c436dfb0cb571
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2022-12-13 16:02:12 +0100

    peparazione bonus

---
+++ bonus/js/script.js
@@ -0,0 +1,53 @@
+//Creazione Array con le informazioni fornite
+let member = [
+    {
+    name: "Wayne Barnett",
+    role: "Founder & Ceo",
+    image: "wayne-barnett-founder-ceo.jpg",
+    },
+    {
+    name: "Angela Caroll",
+    role: "Chief Editor",
+    image: "angella-caroll-chief-editor.jpg",
+    },
+    {
+    name: "Walter Gordon",
+    role: "Office Menager",
+    image: "walter-gordon-office-menager.jpg",
+    },
+    {
+    name: "Angela Lopez",
+    role: "Social Media Menager",
+    image: "angela-lopez-social-media-menager.jpg",
+    },
+    {
+    name: "Scott Estrada",
+    role: "Developer",
+    image: "scott-estrada-developer.jpg",
+    },
+    {
+    name: "Barbara Ramos",
+    role: "Graphic Designer",
+    image: "barbara-ramos-graphic-designer.jpg",
+    },
+]
+
+let data_member = member;
+
+//Milestone 1
+for(let key in data_member){
+    console.log(data_member[key])
+}
+
+//Milestone 2
+const card_item_container = document.querySelector('.cards')
+
+let card_item, container_card;
+
+for(let i=0; i < member.length; i++){
+    card_item = document.createElement('div')
+    card_item.innerText = `Nome: ${member[i].name} 
+    Ruolo: ${member[i].role} 
+    Tag immagine: ${member[i].image}`;
+    card_item_container.append(card_item);
+}
